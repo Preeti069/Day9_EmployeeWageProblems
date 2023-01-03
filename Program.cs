5edcb674182c99f2d20e45e76f10ad7a71d1d39a
preeti <pritich0896@gmail.com>
2023-01-03 13:19:45 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeWages;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocome to Employee Wage Computation Program");
+
+            //CheckAttedance emplayee = new CheckAttedance();
+            //emplayee.Attendence();
+        }
+    }
+}
