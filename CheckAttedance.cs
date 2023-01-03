da5c0f2516c8f609b9b4ffea97259ab566cd55bd
preeti <pritich0896@gmail.com>
2023-01-03 13:22:50 +0530

    CheckAttedance

---
+++ CheckAttedance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    public class CheckAttedance
+    {
+        public void Attendence()
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
