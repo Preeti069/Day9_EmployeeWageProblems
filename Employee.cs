7feb70cc5fe7c6f97fbea230fe80f840d462e1db
preeti <pritich0896@gmail.com>
2023-01-03 13:48:53 +0530

    Add Part Time

--- Employee.cs
+++ Employee.cs
@@ -8,30 +8,32 @@ namespace EmployeeWages
 {
     public class Employee
     {
-        public void Wage()
+        public void PartTTime()
         {
             int IS_FULL_TIME = 1;
-            int empWage = 0;
+            int IS_PART_TIME = 2;
             int empHrs = 0;
             int EMP_RATE_PER_HOUR = 20;
-
+            int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
                 empHrs = 8;
-
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine(": " + empWage);
+            Console.WriteLine("Employe Wage: " + empWage);
         }
     }
 }
